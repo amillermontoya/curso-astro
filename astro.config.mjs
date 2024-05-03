d861715deba70b0c396785c01dc9b11835f5ae3f
DESKTOP-NNAP6OB\amill <amillermontoya@gmail.com>
2024-05-03 19:53:45 -0400

    delete package astro/db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-
-import db from "@astrojs/db";
+import zeabur from '@zeabur/astro-adapter/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), db()],
+  integrations: [tailwind(), preact()],
+  adapter: zeabur(),
   output: 'server'
 });
